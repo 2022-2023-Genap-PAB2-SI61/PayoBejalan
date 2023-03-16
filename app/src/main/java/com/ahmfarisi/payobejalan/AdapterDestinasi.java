02eb8270045cc69b0359eb06ea35428fe9b33f7d
Yohannes Masterous <ahmfarisi@gmail.com>
2023-03-16 18:29:20 +0700

    Update AdapterDestinasi.java

--- app/src/main/java/com/ahmfarisi/payobejalan/AdapterDestinasi.java
+++ app/src/main/java/com/ahmfarisi/payobejalan/AdapterDestinasi.java
@@ -1,12 +1,41 @@
 package com.ahmfarisi.payobejalan;
 
+import android.content.Context;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class AdapterDestinasi {
+import java.util.ArrayList;
+
+public class AdapterDestinasi extends RecyclerView.Adapter<AdapterDestinasi.ViewHolderDestinasi>{
+    private Context ctx;
+    private ArrayList arrNama, arrAlamat, arrJam;
+
+    public AdapterDestinasi(Context ctx, ArrayList arrNama, ArrayList arrAlamat, ArrayList arrJam) {
+        this.ctx = ctx;
+        this.arrNama = arrNama;
+        this.arrAlamat = arrAlamat;
+        this.arrJam = arrJam;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolderDestinasi onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderDestinasi holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     public class ViewHolderDestinasi extends RecyclerView.ViewHolder {
         TextView tvNama, tvAlamat, tvJam;
