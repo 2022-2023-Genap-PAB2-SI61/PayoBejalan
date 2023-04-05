f7f7ce136322de94c1b5a12445de926c8006b723
Yohannes Masterous <ahmfarisi@gmail.com>
2023-04-05 16:55:46 +0700

    Update UbahActivity.java

--- app/src/main/java/com/ahmfarisi/payobejalan/UbahActivity.java
+++ app/src/main/java/com/ahmfarisi/payobejalan/UbahActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class UbahActivity extends AppCompatActivity {
     private EditText etNama, etAlamat, etJam;
@@ -31,5 +33,38 @@ public class UbahActivity extends AppCompatActivity {
         etNama.setText(yNama);
         etAlamat.setText(yAlamat);
         etJam.setText(yJam);
+
+        btnUbah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nama, alamat, jam;
+
+                nama = etNama.getText().toString();
+                alamat = etAlamat.getText().toString();
+                jam = etJam.getText().toString();
+
+                if(nama.trim().equals("")){
+                    etNama.setError("Nama Tidak Boleh Kosong");
+                }
+                else if(alamat.trim().equals("")){
+                    etAlamat.setError("Alamat Tidak Boleh Kosong");
+                }
+                else if(jam.trim().equals("")){
+                    etJam.setError("Jam Tidak Boleh Kosong");
+                }
+                else{
+                    MyDatabaseHelper myDB = new MyDatabaseHelper(UbahActivity.this);
+                    long eks = myDB.ubahData(yId, nama, alamat, jam);
+
+                    if(eks == -1){
+                        Toast.makeText(UbahActivity.this, "Ubah Data Gagal!", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(UbahActivity.this, "Ubah Data Sukses!", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+                }
+            }
+        });
     }
 }
